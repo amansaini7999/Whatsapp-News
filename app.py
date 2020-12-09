a6b60fe77aa202334dff46fd42df3de0c682e351
amansaini7999 <amansaini7999@gmail.com>
2020-12-09 23:37:21 +0530

    try krke dekh raha

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     #return 'Hello, World!'
-    render_template("index.html")
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
