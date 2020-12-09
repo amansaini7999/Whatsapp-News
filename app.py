57ba630791f747bd59ac7ae133e6916b703d9582
amansaini7999 <amansaini7999@gmail.com>
2020-12-09 23:53:14 +0530

    check successfull

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-    #return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
