dfaabf12b219b9a4bf06636f022fd9a5342f1a96
amansaini7999 <amansaini7999@gmail.com>
2020-12-10 19:58:18 +0530

    code modified

--- News/news.py
+++ News/news.py
@@ -1,95 +1,84 @@
-'''import time
+def extract_month(mn):
+    if(mn==1):
+        mon = "jan"
+    
+    elif(mn==2):
+        mon = "feb"
 
-time.localtime(time.time())
+    elif(mn==3):
+        mon = "mar"
 
-mn = time.localtime(time.time()).tm_mon
+    elif(mn==4):
+        mon = "apr"
 
-if(mn==1):
-    mon = "jan"
+    elif(mn==5):
+        mon = "may"
 
-elif(mn==2):
-    mon = "feb"
+    elif(mn==6):
+        mon = "jun"
 
-elif(mn==3):
-    mon = "mar"
+    elif(mn==7):
+        mon = "jul"
 
-elif(mn==4):
-    mon = "apr"
+    elif(mn==8):
+        mon = "aug"
 
-elif(mn==5):
-    mon = "may"
+    elif(mn==9):
+        mon = "sep"
 
-elif(mn==6):
-    mon = "jun"
+    elif(mn==10):
+        mon = "oct"
 
-elif(mn==7):
-    mon = "jul"
+    elif(mn==11):
+        mon = "nov"
 
-elif(mn==8):
-    mon = "aug"
+    elif(mn==12):
+        mon = "dec"
 
-elif(mn==9):
-    mon = "sep"
-
-elif(mn==10):
-    mon = "oct"
-
-elif(mn==11):
-    mon = "nov"
-
-elif(mn==12):
-    mon = "dec"
-
-
-year = time.localtime(time.time()).tm_year
-year
-
-date = time.localtime(time.time()).tm_mday
-date
-
-from bs4 import BeautifulSoup as BS
-
-import requests
-page = requests.get("https://epaper.dailyexcelsior.com/")
-
-pageCount=15
-if(page.status_code==200):
-    soup = BS(page.content, 'html.parser')
-    s = soup.find('div', id="scroller")
-    pageCount=len(s.select('ul', class_='epapertest')[0].select('li'))
-
-pageCount
-
-for i in range(1, pageCount):
-    url = https://epaper.dailyexcelsior.com/epaperpdf/{}/{}/{}{}{}/page{}.pdf.format(year, mon, str(year)[:2], mon, str(date).zfill(2), i)
-    print(url)
-
-from bs4 import BeautifulSoup as BS
-
-import requests
-page = requests.get("https://epaper.dailyexcelsior.com/")
-
-pageCount=15
-if(page.status_code==200):
-    soup = BS(page.content, 'html.parser')
-    s = soup.find('div', id="scroller")
-    pageCount=len(s.select('ul', class_='epapertest')[0].select('li'))
-
-pageCount
-
-
-output = bytearray()
-
-for i in range(1, pageCount):
-    url = https://epaper.dailyexcelsior.com/epaperpdf/{}/{}/{}{}{}/page{}.pdf.format(year, mon, str(year)[:2], mon, str(date).zfill(2), i)
-    #print(url)
-    r = requests.get(url)
-    output.extend(r.content)
-
-f = open("sample1.pdf","wb")
-f.write(output)
-f.close()'''
-
+    return mon
 
 def scrape():
-    return 0
+    import time
+    from PyPDF2 import PdfFileMerger
+    import os
+
+    current_time = time.localtime(time.time())
+
+    mon = extract_month(current_time.tm_mon)
+    year = current_time.tm_year
+    date = current_time.tm_mday
+
+    from bs4 import BeautifulSoup as BS
+    import requests
+    page = requests.get("https://epaper.dailyexcelsior.com/")
+
+    pageCount=15
+    if(page.status_code==200):
+        soup = BS(page.content, 'html.parser')
+        s = soup.find('div', id="scroller")
+        pageCount=len(s.select('ul', class_='epapertest')[0].select('li'))
+
+    for i in range(1, pageCount):
+        url = '''https://epaper.dailyexcelsior.com/epaperpdf/{}/{}/{}{}{}/page{}.pdf'''.format(year, mon, str(year)[:2], mon, str(date).zfill(2), i)
+        r = requests.get(url)
+        f = open("temp/{}.pdf".format(i), "wb")
+        f.write(r.content)
+        f.close()
+        time.sleep(1)
+
+    pdfs = set()
+    for element in os.listdir("temp/"):
+        pdfs.add(int(element.split(".")[0]))
+
+    pdfs = [str(i)+".pdf" for i in list(pdfs)]
+
+    merger = PdfFileMerger()
+    for pdf in pdfs:
+        merger.append("temp/" + pdf)
+
+    merger.write("temp/dailyexcelsior.pdf")
+    merger.close()
+
+    return 1
+
+scrape()
