83229db2bdcd4146e7113ca3d87d9d8f194d4ca9
amansaini7999 <amansaini7999@gmail.com>
2020-12-11 13:45:05 +0530

    final done

--- Whatsapp/whatsapp.py
+++ Whatsapp/whatsapp.py
@@ -1,4 +1,4 @@
-def send_message(pdf_loc, contact_list):
+def send_message(message, pdf_loc, contact_list):
     from selenium import webdriver
     from selenium.webdriver.common.by import By
     from selenium.webdriver.support.ui import WebDriverWait
@@ -10,16 +10,19 @@ def send_message(pdf_loc, contact_list):
     from .config import CHROME_PROFILE_PATH
 
     options = webdriver.ChromeOptions()
+    #options.add_argument('--no-sandbox')
     #options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")      #Heroku
     options.add_argument(CHROME_PROFILE_PATH)
     #options.add_argument("--headless")                                 #Heroku
+    #options.add_argument("--window-size=1440, 900")
+    #options.add_argument("Chrome/85.0.4183.121")
     #options.add_argument("--disable-dev-shm-usage")                    #Heroku
 
     #For Heroku
     #driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
 
     #For Local or EC2
-    driver = webdriver.Chrome("Whatsapp/chromedriver", options=options)    
+    driver = webdriver.Chrome(options=options)    
     driver.get("https://web.whatsapp.com")
     
     try:
@@ -39,12 +42,12 @@ def send_message(pdf_loc, contact_list):
             contact_title.click()
 
             # Send Text
-            '''input_xpath = '//div[@contenteditable="true"][@data-tab="6"]'
+            input_xpath = '//div[@contenteditable="true"][@data-tab="6"]'
             input_box = WebDriverWait(driver, 50).until(
                 EC.presence_of_element_located((By.XPATH, input_xpath))
             )
-            input_box.send_keys("text sent")
-            input_box.send_keys(Keys.ENTER)'''
+            input_box.send_keys(message)
+            input_box.send_keys(Keys.ENTER)
 
             # Send Attachment
             attachment_xpath = '//span[@data-testid="clip"][@data-icon="clip"]'
@@ -54,8 +57,8 @@ def send_message(pdf_loc, contact_list):
             attachment_box.click()
 
             document_box = driver.find_element_by_xpath('//input[@accept="*"]')
-            pdf_loc = os.path.abspath('..') + "/" + pdf_loc + "dailyexcelsior.pdf"
-            document_box.send_keys(pdf_loc)
+            abs_pdf_loc = os.path.abspath('..') + "/" + pdf_loc + "dailyexcelsior.pdf"
+            document_box.send_keys(abs_pdf_loc)
 
             time.sleep(3)
             send_btn = WebDriverWait(driver, 500).until(EC.presence_of_element_located((By.XPATH, '//span[@data-icon="send"]')))
